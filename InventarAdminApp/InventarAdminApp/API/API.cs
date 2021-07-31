ec8a4ab17742229419004c18a70508641c5d6a4d
ILikeSahne <ILikeSahne@gmx.at>
2021-07-31 22:28:39 +0200

    Basic Client Setup, Implemented RSA, First MongoDB test

---
+++ InventarAdminApp/InventarAdminApp/API/API.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarAdminApp
+{
+    class API
+    {
+        private string domain;
+        private int port;
+
+        private TcpClient client;
+        private NetworkStream stream;
+
+        private RSAHelper rsaHelper;
+        private StreamHelper helper;
+
+        private string db, email, pw;
+
+        public API(string _domain, int _port)
+        {
+            domain = _domain;
+            port = _port;
+        }
+
+        public API(int _port)
+        {
+            domain = "127.0.0.1";
+            port = _port;
+        }
+
+        private LoginError OpenConnection()
+        {
+            client = new TcpClient();
+            IAsyncResult result = client.BeginConnect(Dns.GetHostAddresses(domain)[0], port, null, null);
+
+            bool success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+            client.EndConnect(result);
+            if (!success)
+                throw new Error("Couldn't connect to Server!");
+
+            stream = client.GetStream();
+            rsaHelper = new RSAHelper(stream);
+            try
+            {
+                rsaHelper.SetupClient();
+            }
+            catch (Exception e)
+            {
+                WriteLine("Error: {0}", e.ToString());
+            }
+            helper = new StreamHelper(rsaHelper);
+            helper.SendString(db);
+            helper.SendString(email);
+            helper.SendString(pw);
+            return LoginError.NO_ERROR;
+        }
+
+        public void CloseConnection()
+        {
+            if (client != null)
+            {
+                client.Close();
+                client = null;
+            }
+        }
+
+        public LoginError Login(string _db, string _email, string _pw)
+        {
+            db = _db;
+            email = _email;
+            pw = _pw;
+            try
+            {
+                LoginError e = OpenConnection();
+                CloseConnection();
+                return e;
+            } catch (Exception e)
+            {
+                return LoginError.SERVER_NOT_RESPONDING;
+            }
+        }
+
+        public static void WriteLine(string _s, params object[] _args)
+        {
+#if DEBUG
+            Console.WriteLine(_s, _args);
+#endif
+        }
+    }
+
+    public enum LoginError
+    {
+        NO_ERROR, SERVER_NOT_RESPONDING, WRONG_DATABASE, WRONG_USERNAME, WRONG_PASSWORD
+    }
+}
---
+++ InventarAdminApp/InventarAdminApp/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventarAdminApp
+{
+    public partial class Form1 : Form
+    {
+        private API api;
+        public Form1()
+        {
+            InitializeComponent();
+            api = new API(10000);
+            API.WriteLine(api.Login("bulme", "ilikesahne@gmx.at", "Pw123XYZ12").ToString());
+        }
+    }
+}
